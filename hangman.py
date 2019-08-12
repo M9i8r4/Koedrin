ae69bab9bcda3e436441b0f550d9669b6c409a1b
M9i8r4 <53582241+M9i8r4@users.noreply.github.com>
2019-08-12 16:25:29 +0200

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,37 @@
+import random
+import string
+
+words = (horse, carrot, dog, cat, mole, donkey, dragon)
+choosen_word = words in index = random.randint (0, (int(len.words)-1))
+prev_bets = []
+life = 10
+state = [_________________________________,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	|                                 |,
+	 _________________________________,]
+
+def print_state(life):
+	print (state)
+	print (life + "LIFE LEFT!")
+
+while life > 0
+	print("You already asked: " + prev_bets)
+	bet = string(input("Bet a letter!\n")
+		if len.bet == 1 and bet.asciletter:
+			
+		else:
+			print ("Your bet is invalid! Try again.)
+			continue
+
+	
